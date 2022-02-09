872469508d1a8777cb72653ce3efcc9982e261f3
Andrea Stewart <55840298+als329@users.noreply.github.com>
2022-02-09 17:28:25 -0500

    Adding booster data
    updating get_vax function to pull in additional indicators (including booster)

--- Rfunctions/get_vax_data.R
+++ Rfunctions/get_vax_data.R
@@ -27,15 +27,19 @@ function(rfunctions.dir) {
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   
   map_data_field <- setNames(c("Cumulative Doses Administered", "Cumulative Doses Administered", "Cumulative People Vaccinated (1 or more)", "Cumulative People Vaccinated (1 or more)",
-                               "Cumulative People Vaccinated (Fully)", "Cumulative People Vaccinated (Fully)", "Daily Doses Administered (7-day average)", "Daily Doses Administered (7-day average)"),
+                               "Cumulative People Vaccinated (Fully)", "Cumulative People Vaccinated (Fully)", "Daily Doses Administered (7-day average)", "Daily Doses Administered (7-day average)",
+                              "Daily People Vaccinatd  (7-day average)", "Daily People Vaccinated (7-day average)", "Total Boosters", "Total Boosters"),
                              c("total_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated", "people_vaccinated_per_hundred",
-                               "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million"))
-  map_count_or_rate <- setNames(c("Count", "Rate", "Count", "Rate", "Count", "Rate", "Count", "Rate"),
+                               "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million",
+                               "daily_people_vaccinaed","daily_people_vaccinated_per_hundred","total_boosters","total_boosters_per_hundred"))
+  map_count_or_rate <- setNames(c("Count", "Rate", "Count", "Rate", "Count", "Rate", "Count", "Rate","Count", "Rate", "Count", "Rate"),
                                 c("total_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated", "people_vaccinated_per_hundred",
-                                  "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million"))
-  map_data_suffix <- setNames(c(" ", "Per 100 people", " ", "Per 100 people", " ", "Per 100 people", " ", "Per 1M people"),
+                                  "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million",
+                                  "daily_people_vaccinaed","daily_people_vaccinated_per_hundred","total_boosters","total_boosters_per_hundred"))
+  map_data_suffix <- setNames(c(" ", "Per 100 people", " ", "Per 100 people", " ", "Per 100 people", " ", "Per 1M people"," ", "Per 100 people", " ", "Per 100 people"),
                               c("total_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated", "people_vaccinated_per_hundred",
-                                "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million"))
+                                "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million",
+                                "daily_people_vaccinaed","daily_people_vaccinated_per_hundred","total_boosters","total_boosters_per_hundred"))
   
   # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # ~~~~~~~~~~~~~~~~ Function to generate datasets ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -86,22 +90,22 @@ function(rfunctions.dir) {
   loc$vaccine_manufacturer <- str_trim(loc$vaccine_manufacturer)
     
   vax_all.long <- vax_all %>% 
-    gather("raw_field", "data_value", total_vaccinations:daily_vaccinations_per_million)
+    gather("raw_field", "data_value", total_vaccinations:daily_people_vaccinated_per_hundred)
   vax_all.long$data_field <- map_data_field[vax_all.long$raw_field]
   vax_all.long$data_suffix <- map_data_suffix[vax_all.long$raw_field]
   vax_all.long$count_or_rate <- map_count_or_rate[vax_all.long$raw_field]
   vax_all.long$ou_date_match <- paste(vax_all.long$iso_code, vax_all.long$date, sep="_")
 
-  count_vaccinated_filter <- function(df) { return(df$raw_field == "total_vaccinations" | df$raw_field == "people_vaccinated" | df$raw_field == "people_fully_vaccinated")}
+  count_vaccinated_filter <- function(df) { return(df$raw_field == "total_vaccinations" | df$raw_field == "people_vaccinated" | df$raw_field == "people_fully_vaccinated" | df$raw_field =="total_boosters")}
   count_vaccinated_breaks <- c(0,1e3,1e4,1e5,1e6,1e7,Inf)
   count_vaccinated_ticks <- c(1e3,1e4,1e5,1e6,1e7,5e8)
-  rate_vaccinated_filter <- function(df) { return(df$raw_field == "people_vaccinated_per_hundred" | df$raw_field == "people_fully_vaccinated_per_hundred")}
+  rate_vaccinated_filter <- function(df) { return(df$raw_field == "people_vaccinated_per_hundred" | df$raw_field == "people_fully_vaccinated_per_hundred"| df$raw_field == "total_boosters_per_hundred")}
   rate_vaccinated_breaks <- c(0,0.05,1,5,10,20,Inf)
   rate_vaccinated_ticks <- c(0.05,1,5,10,20,100)
-  rate_doses_filter <- function(df) { return(df$raw_field == "total_vaccinations_per_hundred")}
+  rate_doses_filter <- function(df) { return(df$raw_field == "total_vaccinations_per_hundred" | df$raw_field == "daily_people_vaccinated_per_hundred")}
   rate_doses_breaks <- c(0,0.05,1,5,10,20,Inf)
   rate_doses_ticks <- c(0.05,1,5,10,20,200)
-  count_daily_filter <- function(df) { return(df$raw_field == "daily_vaccinations")}
+  count_daily_filter <- function(df) { return(df$raw_field == "daily_vaccinations"| df$raw_field =="daily_people_vaccinated")}
   count_daily_breaks <- c(0,10,1e2,1e3,1e4,1e5,Inf)
   count_daily_ticks <- c(10,1e2,1e3,1e4,1e5,5e6)
   rate_daily_filter <- function(df) { return(df$raw_field == "daily_vaccinations_per_million")}
@@ -121,8 +125,8 @@ function(rfunctions.dir) {
   
   cats = expand.grid(cat_num = c(1:6),
                      raw_field = c("total_vaccinations", "total_vaccinations_per_hundred", "people_vaccinated", "people_vaccinated_per_hundred",
-                       "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million")
-                     )
+                       "people_fully_vaccinated", "people_fully_vaccinated_per_hundred", "daily_vaccinations", "daily_vaccinations_per_million","daily_people_vaccinated", "daily_people_vaccinated_per_hundred",
+                     "total_boosters","total_boosters_per_hundred"))
   
   cats$data_field <- map_data_field[cats$raw_field]
   cats$data_suffix <- map_data_suffix[cats$raw_field]
